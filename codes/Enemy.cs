10078c5448b233aa44704f5c5bdc71c25e381b9e
ADARSH_VULLI <43677033+ADARSHULTIMATE@users.noreply.github.com>
2020-05-09 12:36:58 +0530

    Add files via upload

---
+++ codes/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]private GameObject _cloudParticlePrefab;
+
+     private void  OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Instantiate(_cloudParticlePrefab,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+        Enemy enmey = collision.collider.GetComponent<Enemy>();
+        if (enmey != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+}
